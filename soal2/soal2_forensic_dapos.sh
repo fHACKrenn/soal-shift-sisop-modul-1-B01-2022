25a8b07987db0e7774d5d7e90e365a0e6f1d9067
Fidhia Ainun Khofifah <90237196+fidhiaakaa@users.noreply.github.com>
2022-02-26 16:12:08 +0700

    2c 2d 2e

--- soal2/soal2_forensic_dapos.sh
+++ soal2/soal2_forensic_dapos.sh
@@ -11,16 +11,16 @@ mkdir "$root"/forensic_log_website_daffainfo_log
 awk 'BEGIN {FS=":";max=0;ipmax="";}
 {
   if(NR>1){
-    a[$1]++
+    ip[$1]++ #menyimpan IP ke array
   }
 }
 END {
-  max=0
+  max=0 #set 0 untuk perbandingan
   ipmax=""
-  for (b in a){
-    if(a[b]>max){ #cek tiap IP dengan banyak request
+  for (b in ip){
+    if(ip[b]>max){ #cek tiap IP dengan banyak request
       ipmax=b
-      max=a[b]
+      max=ip[b]
     }
   }
   print "IP yang paling banyak mengakses server adalah:", ipmax, "sebanyak", max, "requests\n"
@@ -29,8 +29,20 @@ END {
 
 #2d
 awk 'BEGIN {FS=":";n=0;}
-/curl/ {++n}
+/curl/ {++n} #cek curl tiap row, lalu di count
 END {
   print "Ada", n, "requests yang menggunakan curl sebagai user-agent\n"
+}
+ ' log_website_daffainfo.log >> "$root"/forensic_log_website_daffainfo_log/result.txt
+
+#2e
+awk 'BEGIN {FS=":";}
+{
+ if(NR>1 && $2~"22/Jan/2022:02"){ #cari string yang memuat 22/Jan/2022:02
+  ip[$1]=ip[$1]+1
+ }
+}
+END {
+  for(hasil in ip) printf("%s\n", hasil)
 }
  ' log_website_daffainfo.log >> "$root"/forensic_log_website_daffainfo_log/result.txt
