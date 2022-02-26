e1e2403c65f2db7720bb880cdeab05e16fc6fdd5
Fidhia Ainun Khofifah <90237196+fidhiaakaa@users.noreply.github.com>
2022-02-26 15:13:58 +0700

    update 2c 2d

--- soal2/soal2_forensic_dapos.sh
+++ soal2/soal2_forensic_dapos.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
 
+root=/home/fidhiaaka/praktikum2
+
+#2a
+mkdir "$root"/forensic_log_website_daffainfo_log
+
+#2b
+
+#2c
+awk 'BEGIN {FS=":";max=0;ipmax="";}
+{
+  if(NR>1){
+    a[$1]++
+  }
+}
+END {
+  max=0
+  ipmax=""
+  for (b in a){
+    if(a[b]>max){ #cek tiap IP dengan banyak request
+      ipmax=b
+      max=a[b]
+    }
+  }
+  print "IP yang paling banyak mengakses server adalah:", ipmax, "sebanyak", max, "requests\n"
+}
+ ' log_website_daffainfo.log > "$root"/forensic_log_website_daffainfo_log/result.txt
+
+#2d
+awk 'BEGIN {FS=":";n=0;}
+/curl/ {++n}
+END {
+  print "Ada", n, "requests yang menggunakan curl sebagai user-agent\n"
+}
+ ' log_website_daffainfo.log >> "$root"/forensic_log_website_daffainfo_log/result.txt
